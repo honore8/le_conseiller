e16748f8901ead268979b7c1135c324580b4958a
ritaagbagbati <31415635+ritaagbagbati@users.noreply.github.com>
2020-06-26 18:11:25 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,62 @@
+
+@extends('layout')
+
+@section('content')
+    <div class="container">
+        <br><br>
+        
+
+        <div class="col-12">
+              <div class="row  "> 
+                    <div class="col-9">
+                        <div class="row  ">   
+                            <div class=" col-3 " ><hr class="ligne"></hr> </div>
+                            <div class=" col-3"  style="text-align: center " > A LA UNE </div>
+                            <div class=" col-3 " ><hr class="ligne"></hr> </div>
+                        </div>
+                    </div>
+               </div>
+             <div class="row  "> 
+                    <div class="col-9">
+                        <div class="row  ">   
+                            <div class=" col-6 " > <img class="mx-auto" src="{{asset('images/conseiller.png')}}" height="100px" width="380px"></div>
+                          
+                            <div class=" col-3 " >  <img class="mx-auto" src="{{asset('images/conseiller.png')}}" height="100px" width="200px"> </div>
+                        </div>
+                    </div>
+             </div>
+
+             <div class="row  "> 
+                    <div class="col-9">
+                        <div class="row  ">   
+                            <div class=" col-3 " > <hr class="ligne"></hr> </div>
+                            <div class=" col-3"  style="text-align: center " > AUX DECIDEURS </div>
+                            <div class=" col-3 " ><hr class="ligne"></hr> </div>
+                        </div>
+                    </div>
+               </div>
+             <div class="row  "> 
+                    <div class="col-9">
+                        <div class="row  ">   
+                            <div class=" col-2 " > <img class="mx-auto" src="{{asset('images/Image-interface-2.png')}}" height="100px" width="150px"></div>
+                            <div class=" col-2 " > <img class="mx-auto" src="{{asset('images/Image-interface-2.png')}}" height="100px" width="150px"></div>
+                            <div class=" col-2 " > <img class="mx-auto" src="{{asset('images/Image-interface-2.png')}}" height="100px" width="150px"></div>
+                            <div class=" col-3 " >  <img class="mx-auto" src="{{asset('images/Image-interface-2.png')}}" height="100px" width="150px"> </div>
+                        </div>
+                    </div>
+             </div>
+             <div class="row  "> 
+                    <div class="col-9">
+                        <div class="row  ">   
+                            <div class=" col-3 " ><hr class="ligne"></hr> </div>
+                            <div class=" col-3"  style="text-align: center " >RUBRIQUES </div>
+                            <div class=" col-3 " > <hr class="ligne"></hr></div>
+                        </div>
+                    </div>
+               </div>
+                    </div>
+                        <div class="col-3">  </div> 
+                    </div>
+        </div>
+    </div>
+@endsection
